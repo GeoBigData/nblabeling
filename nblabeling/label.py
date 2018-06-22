64305c46cc5c9a7c54541b5438070d32d06cf29c
mgleason <michael.gleason@digitalglobe.com>
2018-06-22 16:32:10 -0600

    fixing issue with blm and indexing

--- nblabeling/label.py
+++ nblabeling/label.py
@@ -77,7 +77,7 @@ class LabelSegment(object):
 
         row_start, row_stop, col_start, col_stop = self.buffered_bbox(pixel_buffer)
 
-        return self.image[:, row_start:row_stop, col_start:col_stop].base_layer_match(blm=blm)
+        return self.image.base_layer_match(blm=blm)[:, row_start:row_stop, col_start:col_stop]
 
     def pan(self, pixel_buffer=50, equalize_histogram=True):
 
